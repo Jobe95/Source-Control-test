9d91b4e5dfa337262b0c0453143d3c3b56b4af21
Jonatan Bengtsson <jonatan.bengtsson@entrydraft.se>
2020-08-09 12:32:37 +0200

    Initial Commit

---
+++ Source Control first/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Source Control first
+//
+//  Created by Jonatan Bengtsson on 2020-08-09.
+//  Copyright © 2020 Jonatan Bengtsson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Source Control first/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Source Control first
+//
+//  Created by Jonatan Bengtsson on 2020-08-09.
+//  Copyright © 2020 Jonatan Bengtsson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
